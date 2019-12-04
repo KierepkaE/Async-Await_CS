d2f9f179cd9940286d7800f1f8af8e4cdeab37f3
kierepkae <kierepkaedyta@gmail.com>
2019-12-04 16:28:38 +0000

    rewrite to use await

--- Program.cs
+++ Program.cs
@@ -2,21 +2,21 @@
 
 namespace Async_Await {
     class Program {
-        static void Main (string[] args) {
+        static async Task Main (string[] args) {
             Coffee cup = PourCoffee ();
             Console.WriteLine ("coffee is ready");
-            Egg eggs = FryEggs (2);
+            Egg eggs = await FryEggs (2);
             Console.WriteLine ("eggs are ready");
-            Bacon bacon = FryBacon (3);
+            Bacon bacon = await FryBacon (3);
             Console.WriteLine ("bacon is ready");
-            Toast toast = ToastBread (2);
+            Toast toast = await ToastBread (2);
             ApplyButter (toast);
             ApplyJam (toast);
             Console.WriteLine ("toast is ready");
             Juice oj = PourOJ ();
             Console.WriteLine ("oj is ready");
-            Console.WriteLine ("Breakfast is ready!");
 
+            Console.WriteLine ("Breakfast is ready!");
         }
     }
 }
