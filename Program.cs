23eec1b0736f02c05af1e39b1ec91dd6a35249b1
kierepkae <kierepkaedyta@gmail.com>
2019-12-04 16:31:18 +0000

    rewrite blocking example of code in the comment

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 
 namespace Async_Await {
     class Program {
+
+        // ASYNC
+
         static async Task Main (string[] args) {
             Coffee cup = PourCoffee ();
             Console.WriteLine ("coffee is ready");
@@ -18,5 +21,23 @@ namespace Async_Await {
 
             Console.WriteLine ("Breakfast is ready!");
         }
+
+        // BLOCKING
+        // static void Main (string[] args) {
+        //     Coffee cup = PourCoffee ();
+        //     Console.WriteLine ("coffee is ready");
+        //     Egg eggs = FryEggs (2);
+        //     Console.WriteLine ("eggs are ready");
+        //     Bacon bacon = FryBacon (3);
+        //     Console.WriteLine ("bacon is ready");
+        //     Toast toast = ToastBread (2);
+        //     ApplyButter (toast);
+        //     ApplyJam (toast);
+        //     Console.WriteLine ("toast is ready");
+        //     Juice oj = PourOJ ();
+        //     Console.WriteLine ("oj is ready");
+        //     Console.WriteLine ("Breakfast is ready!");
+
+        // }
     }
 }
